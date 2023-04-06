424172a5800dbd6ccc1771737b7a073273aff00d
chirutheboss <85287194+chirutheboss@users.noreply.github.com>
2023-04-06 20:37:21 +0530

    Update start_constants.py

--- TelegramBot/helpers/start_constants.py
+++ TelegramBot/helpers/start_constants.py
@@ -23,8 +23,8 @@ ABOUT_CAPTION = f"""• Python version : {__python_version__}
 • Bot version : {__version__}
 • pyrogram  version : {__pyro_version__}
 
-**Github Repo**: https://github.com/sanjit-sinha/Tg-MediaInfoBot"""
+**🧑‍💻DEVELOPER🧑‍💻**: @chiru_wayne"""
 
-START_ANIMATION = "https://telegra.ph/file/c0857672b427bec8542f6.mp4"
+START_ANIMATION = "https://te.legra.ph/file/c0857672b427bec8542f6.mp4"
 
-START_CAPTION = """ Hello There! I am a Telegram Bot which can generate screenshots from video files, trim sample video files, and create mediainfo for Telegram files, Direct download links and Google Drive links.\n\nPress commands button to know more about bot commands and its usage."""
+START_CAPTION = """👋Hello There! I am a Telegram Bot which can generate screenshots from video files, trim sample video files, and create mediainfo for Telegram files, Direct download links and Google Drive links👋.\n\nPress commands button to know more about bot commands and its usage."""
